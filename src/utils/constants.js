a9463370f48193ba2b919e8998fe64d4167abab5
Akhilleshgoswami <agiri5375@gmail.com>
2021-11-25 08:45:05 +0530

    add functionlity to get value form menuitems

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,35 +1,36 @@
 export const MARKS = [
   {
     value: 0,
-    label: "0%"
+    label: "0%",
   },
   {
     value: 25,
-    label: "25%"
+    label: "25%",
   },
   {
     value: 50,
-    label: "50%"
+    label: "50%",
   },
   {
     value: 75,
-    label: "75%"
+    label: "75%",
   },
   {
     value: 100,
-    label: "100%"
-  }
+    label: "100%",
+  },
 ];
 
 export const VIEW = {
   protect: {
     label: "Protect",
-    route: "/"
+    route: "/",
   },
   buy: {
     label: "Buy",
     route: "/buy"
   }
+
 };
 
 export const ACTIVITY_HISTORY = [
@@ -39,7 +40,7 @@ export const ACTIVITY_HISTORY = [
     usdcPriceLimit: 3841.28,
     ethPriceLimit: 0.07,
     orderDate: "14th Sept, 2021",
-    orderTime: "18:22 IST"
+    orderTime: "18:22 IST",
   },
   {
     type: "Buy",
@@ -47,7 +48,7 @@ export const ACTIVITY_HISTORY = [
     usdcPriceLimit: 3841.28,
     ethPriceLimit: 0.07,
     orderDate: "14th Sept, 2021",
-    orderTime: "18:22 IST"
+    orderTime: "18:22 IST",
   },
   {
     type: "Protect",
@@ -55,7 +56,7 @@ export const ACTIVITY_HISTORY = [
     usdcPriceLimit: 3841.28,
     ethPriceLimit: 0.07,
     orderDate: "14th Sept, 2021",
-    orderTime: "18:22 IST"
+    orderTime: "18:22 IST",
   },
   {
     type: "Protect",
@@ -63,6 +64,21 @@ export const ACTIVITY_HISTORY = [
     usdcPriceLimit: 3841.28,
     ethPriceLimit: 0.07,
     orderDate: "14th Sept, 2021",
-    orderTime: "18:22 IST"
-  }
+    orderTime: "18:22 IST",
+  },
+];
+export const ASSETS_VALUE = [
+  // we can put assets address on the place of assetsname
+
+  "eth",
+
+  "link",
+
+  "usdc",
+
+  "wbtc",
+
+  "uni",
+
+  "weth",
 ];
