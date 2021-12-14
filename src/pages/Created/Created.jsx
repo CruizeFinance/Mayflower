8402f714140f053f1d36de34a33886c9e33827d0
LilFatFrank <karan.parsnani@ymail.com>
2021-12-14 12:25:34 +0530

    redirect changes and meta description

--- src/pages/Created/Created.jsx
+++ src/pages/Created/Created.jsx
@@ -1,6 +1,17 @@
 import { CreatedDetails, HistoryBox, InfoBox } from "../Sections";
+import { useWeb3React } from "@web3-react/core";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Created = (props) => {
+  const navigate = useNavigate();
+  const { active } = useWeb3React();
+
+  /* redirect back to home, if the wallet is not connected. */
+  useEffect(() => {
+    if (!active) navigate(`/`);
+  }, [active]);
+
   return (
     <>
       <CreatedDetails
